df74400588a89839e28c33d09456cdd6ff7f0eda
OnlyLuffy <319377758@qq.com>
2025-06-12 15:15:46 +0800

    feature: implify sarsa base

--- simplerl/algorithms/sarsa/sarsa.py
+++ simplerl/algorithms/sarsa/sarsa.py
@@ -3,12 +3,19 @@ from typing import Choice
 import numpy as np
 
 class SarsaBase(ABC):
-    def __init__(self, env, gamma=0.99, epsilon=0.01, alpha=0.01, num_episodes=1000):
+    def __init__(self, env, gamma=0.99, epsilon=0.01, alpha=0.01):
+        """
+        Initialize the Sarsa algorithm
+        Args:
+            env: The environment
+            gamma: The discount factor
+            epsilon: The exploration rate
+            alpha: The learning rate
+        """
         self.env = env
         self.gamma = gamma
         self.epsilon = epsilon
         self.alpha = alpha
-        self.num_episodes = num_episodes
         
         assert hasattr(self.env, "_get_q_table"), "The environment must have a _get_q_table method"
         assert hasattr(self.env, "_get_q_index"), "The environment must have a _get_q_index method"
@@ -30,10 +37,33 @@ class SarsaBase(ABC):
         pass
 
 class Sarsa(SarsaBase):
-    def __init__(self, env, gamma=0.99, epsilon=0.01, alpha=0.01, num_episodes=1000):
-        super().__init__(env, gamma, epsilon, alpha, num_episodes)
+    def __init__(self, env, gamma=0.99, epsilon=0.01, alpha=0.01, n_steps=1000):
+        super().__init__(env, gamma, epsilon, alpha)
+        self.n_steps = n_steps
         
+
     def learn(self):
-        pass
+        obs, _ = self.env.reset()
+        # sample a_t
+        action = self.epsilon_greedy_policy(obs)
+        
+        for _ in range(self.n_steps):
+            # take action
+            next_obs, reward, terminated, truncated, info = self.env.step(action)
+            
+            # sample a_t+1
+            next_action = self.epsilon_greedy_policy(next_obs)
+            
+            # update Q by (obs, action, reward, next_obs, next_action)
+            q_sa = self.Q[self.env._get_q_index(obs, action)]
+            q_sa_next = self.Q[self.env._get_q_index(next_obs, next_action)]
+            q_sa = q_sa + self.alpha * (reward + self.gamma * q_sa_next - q_sa)
+            
+            # update obs, action
+            obs = next_obs
+            action = next_action
+            
+            
+            
 
 
